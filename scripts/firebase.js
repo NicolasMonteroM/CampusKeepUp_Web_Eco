893311e2fb9c0fc7f106d14b45458dae3bf767b0
NicolasMonteroM <nicolasmontero42@gmail.com>
2021-06-14 22:45:31 -0500

    Alguito

---
+++ scripts/firebase.js
@@ -0,0 +1,12 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBcu3p_t4u3kYws5N3yTbp0XLpMBUqCaO8",
+    authDomain: "campuskeepup.firebaseapp.com",
+    databaseURL: "https://campuskeepup-default-rtdb.firebaseio.com",
+    projectId: "campuskeepup",
+    storageBucket: "campuskeepup.appspot.com",
+    messagingSenderId: "13026235560",
+    appId: "1:13026235560:web:c107670d7712b988709e9d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
